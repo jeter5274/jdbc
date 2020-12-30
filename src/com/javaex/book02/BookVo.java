e5b5f4c191b2f28bf28966975bdb4b5519df0888
jeter5274 <jeter5274@gmail.com>
2020-12-30 14:34:12 +0900

    상속제거 등 정리

--- src/com/javaex/book02/BookVo.java
+++ src/com/javaex/book02/BookVo.java
@@ -1,13 +1,12 @@
 package com.javaex.book02;
 
 import java.sql.Date;
-import java.util.List;
 
 public class BookVo{
 
 	//필드
 	public int bookId, authorId;
-	public String title, pubs, authorName, authorDesc;
+	public String title, pubs, authorName, authorDesc;	//authorName, authorDesc 추가
 	public Date pub_date;
 	
 	//생성자
@@ -19,14 +18,9 @@ public class BookVo{
 		this.title = title;
 		this.pubs = pubs;
 		this.pub_date = java.sql.Date.valueOf(pub_date);
-		
-		//작가정보를 가져옴
-		AuthorDao authorDao = new AuthorDao();
-		List<AuthorVo> authorVoList = authorDao.getAuthorList();
-		authorName = authorVoList.get(authorId-1).authorName;
-		authorDesc = authorVoList.get(authorId-1).authorDesc;
 	}
-	//BookVo.searchBook에서 사용
+	
+	//bookDao.searchBook에서 사용
 	public BookVo(int bookId, String title, String pubs, Date pub_date, String authorName) {
 		this.bookId = bookId;
 		this.title = title;
@@ -42,15 +36,9 @@ public class BookVo{
 		this.title = title;
 		this.pubs = pubs;
 		this.pub_date = java.sql.Date.valueOf(pub_date);
-		
-		//작가정보를 가져옴(작가정보를 수정할 경우를 대비)
-		AuthorDao authorDao = new AuthorDao();
-		List<AuthorVo> authorVoList = authorDao.getAuthorList();
-		authorName = authorVoList.get(authorId-1).authorName;
-		authorDesc = authorVoList.get(authorId-1).authorDesc;
 	}
 	
-	//BookVo.bookInsert에서 사용
+	//bookDao.getBookList에서 사용
 	public BookVo(int bookId, String title, String pubs,Date pub_date, int authorId, String authorName, String authorDesc) {
 		this.bookId = bookId;
 		this.authorId = authorId;
@@ -94,7 +82,19 @@ public class BookVo{
 	public void setPub_date(Date pub_date) {
 		this.pub_date = pub_date;
 	}
-	
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getAuthorDesc() {
+		return authorDesc;
+	}
+	public void setAuthorDesc(String authorDesc) {
+		this.authorDesc = authorDesc;
+	}
+
 	//메소드 - 일반
 	@Override
 	public String toString() {
